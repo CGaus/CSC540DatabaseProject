b89baab32543fe9737a4dbe5d1de9708b270c6c8
DasGaus <chrisgaus@gmail.com>
2018-11-12 04:19:45 -0500

    added employee files, paths/file names will need to be updated.

---
+++ .idea/project_files/employee/portal.php
@@ -0,0 +1,15 @@
+<?php
+require "database.php";
+session_start();
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+    echo "Welcome {$_SESSION['username']}";
+}
+echo "</br>";
+echo "</br>";
+echo "</br>";
+echo "Employee Portal Page";
+echo "</br>";
+echo "</br>";
+echo "<a href='logout.php'>Logout</a>";
+?>
